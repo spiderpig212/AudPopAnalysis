fa9c7760327c0f3e86629f3e743ad544c66ba270
Matt Nardoci <mattnardoci@gmail.com>
2025-08-05 13:59:52 -0700

    Removing deleted files from git branch

--- Archive/encoding_exploration.py
+++
@@ -1,8 +0,0 @@
-"""
-This will cover most of the code used for the creation of a prelim figure for
-encoding. I plan to complete the following:
-1.) For each paradigm, go through and calculate the FR under each trial condition, averaged acorss trials of that condition
-2.) Calculate a p-value for if firing rate changes significantly from baseline under each condition
-3.) Calculate for each paradigm
-4.) Calculate which fraction of cells are mixed and pure selectivity for each brain region
-"""
